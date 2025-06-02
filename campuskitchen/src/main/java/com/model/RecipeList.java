cb4416a09a602ce7b5d307539660cad2846ef80e
KimberlyB6 <brownks6@g.cofc.edu>
2025-06-02 13:27:29 -0400

    recipeList

--- campuskitchen/src/main/java/com/model/RecipeList.java
+++ campuskitchen/src/main/java/com/model/RecipeList.java
@@ -1,5 +1,32 @@
 package com.model;
 
+import java.util.ArrayList;
+
 public class RecipeList {
-    
+    private ArrayList<Recipe> recipes;
+    private RecipeList recipeList;
+
+    private RecipeList(ArrayList<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+
+    public void getInstance(ArrayList<Recipe> recipes) {
+
+    }
+
+    public void addRecipe(String name, String description, int duration, ArrayList<String> steps, ArrayList<Ingredients> ingredients, ArrayList<String> categories, User author, RecipeStatus status) {
+
+    }
+
+    public void getRecipe(String keyword) {
+        
+    }
+
+    public void editRecipe(String id, String name, String description, int duration, ArrayList<String> steps, ArrayList<Ingredients> ingredients, ArrayList<String> categories, User author, RecipeStatus status) {
+        
+    }
+
+    public void deleteRecipe(String id) {
+        
+    }
 }
