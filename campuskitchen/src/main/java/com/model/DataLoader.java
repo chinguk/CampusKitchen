b456bc7958661a1ec6bee970964c24cd31bbc3b9
Firstname Lastname <yoursignupgithubemail@domain.com>
2025-06-02 13:38:18 -0400

    commit

---
+++ campuskitchen/src/main/java/com/model/DataLoader.java
@@ -0,0 +1,40 @@
+package com.model;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.UUID;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+
+import java.util.ArrayList;
+
+public class DataLoader {
+    
+
+    public static ArrayList<User> getUsers(){
+
+    }
+
+    public static ArrayList<Recipe> getRecipes(){
+
+    }
+
+    public static ArrayList<MealPlan> getMealPlans(){
+
+    }
+
+    public static ArrayList<Ingredients> getIngredients(){
+
+    }
+
+    public static rrayList<Admin> getAdmin(){
+
+    }
+
+    public static ArrayList<Recipe> getRecipes(){
+
+    }
+}
--- campuskitchen/src/main/java/module-info.java
+++ campuskitchen/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.campus {
     requires javafx.controls;
     requires javafx.fxml;
+    requires json.simple;
 
     opens com.campus to javafx.fxml;
     exports com.campus;
