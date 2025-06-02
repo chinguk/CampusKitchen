e190f492658c0b604c7da04a1e5e28d7d809912e
KimberlyB6 <brownks6@g.cofc.edu>
2025-06-02 12:50:04 -0400

    rating

---
+++ campuskitchen/src/main/java/com/model/Ingredients.java
@@ -0,0 +1,22 @@
+package com.model;
+
+public class Ingredients extends Recipe {
+    
+    private String name;
+    private double amount;
+    private Unit unit;
+    
+    public Ingredients (String name, double amount, Unit unit) {
+        this.name = name;
+        this.amount = amount;
+        this.unit = unit;
+    }
+
+    public void updateAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void updateUnit(Unit unit) {
+        this.unit = unit;
+    }
+}
