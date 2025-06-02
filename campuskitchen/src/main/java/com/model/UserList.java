d330dd5010077ee0459be0c47fc91b78c7b0f2c3
Firstname Lastname <yoursignupgithubemail@domain.com>
2025-06-02 13:23:56 -0400

    UserList added

---
+++ campuskitchen/src/main/java/com/model/UserList.java
@@ -0,0 +1,32 @@
+package com.model;
+
+import java.util.ArrayList;
+
+public class UserList {
+    private ArrayList<User> users;
+    private UserList userList;
+
+    public UserList(){
+        
+    }
+
+    public UserList getInstance(){
+
+    }
+
+    public void addUser(String firstName, String lastName, String email, String universityID, String username, String password){
+
+    }
+
+    public User getUser(String username){
+
+    }
+
+    public void editUser(String firstName, String lastName, String email, String universityID, String username, String password){
+
+    }
+
+    public void saveUsers(User user){
+
+    }
+}
