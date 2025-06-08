9081ac4c3066d1ad476f875d73dee16c2bd812bd
Chi Nguyen <chikn@email.sc.edu>
2025-06-08 18:16:35 -0400

    added documentation for RecipeSystemFACADE #71

--- campuskitchen/src/main/java/com/model/RecipeSystemFACADE.java
+++ campuskitchen/src/main/java/com/model/RecipeSystemFACADE.java
@@ -7,39 +7,78 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * Facade for recipe system that handles account management, recipe operations,
+ * meal plan creation, and grocery list generation. Implements singleton pattern
+ */
 public class RecipeSystemFACADE {
     private static final User NEW_USER = null;
     private User user;
     private static RecipeSystemFACADE recipeSystemFACADE;
 
+    /**
+     * Private constructor to enforce singleton pattern
+     */
     private RecipeSystemFACADE() {}
 
+    /**
+     * Returns singleton instance of facade and creates it if necessary
+     * @return single RecipeSystemFACADE instance
+     */
     public static RecipeSystemFACADE getInstance() {
         if(recipeSystemFACADE == null) {
             recipeSystemFACADE = new RecipeSystemFACADE();
         }
         return recipeSystemFACADE;
     }
+
+    /**
+     * Create new user account in the system
+     * @param firstName user's first name
+     * @param lastName user's last name
+     * @param email user's email
+     * @param universityID user's ID
+     * @param username user's username
+     * @param password user's password
+     * @return True if creates account, false otherwise
+     */
     public boolean createAccount(String firstName, String lastName, String email, String universityID, String username, String password) {
         return UserList.getInstance().addUser(firstName, lastName, email, universityID, username, password);
     }
 
+    /**
+     * Updates profile information of currently logged in user
+     * @param user Updated User
+     */
     public void updateProfile(User user) {
 
     }
 
+    /**
+     * Logs user into system
+     * @param username Username to authenticate
+     * @param password Password to authenticate
+     * @return User if login succeeds, null otherwise
+     */
     public User login(String username, String password) {
         return UserList.getInstance().getUser(username);
     }
 
+    /**
+     * Retrieves recipe matching keyword
+     * @param word Search word
+     * @return matching recipe or null if not found
+     */
     public Recipe getRecipeByKeyWord(String word) {
         return null;
-
     }
 
+    /**
+     * Returns all recipes in the system
+     * @return All recipes
+     */
     public ArrayList<Recipe> getAllRecipe() {
         return null;
-
     }
 
     public void submitRecipe(String name, String description, int duration, ArrayList<String> steps, ArrayList<Ingredient> ingredients, ArrayList<Course> course, ArrayList<Culture> culture, ArrayList<Dietary> dietary, User author, RecipeStatus status) {
@@ -64,9 +103,13 @@ public class RecipeSystemFACADE {
 
     public double getAverageRating(Recipe recipe) {
         return 0;
-
     }
 
+    /**
+     * Creates new meal plan for logged in user and generates grocery list file
+     * @param name Name of meal plan
+     * @param recipes list of recipes to include
+     */
     public void createMealPlan(String name, ArrayList<Recipe> recipes) {
     User currentUser = this.user;
 
@@ -85,6 +128,11 @@ public class RecipeSystemFACADE {
         return null;
     }
 
+    /**
+     * Generates grocery list for recipes in meal plans
+     * @param mealPlan Meal plan to generate list for
+     * @return List of ingredients
+     */
     public List<Ingredient> generateGroceryList(MealPlan mealPlan) {
         if (mealPlan == null) {
             return null;
@@ -94,6 +142,11 @@ public class RecipeSystemFACADE {
         return groceryList;        
     }
 
+    /**
+     * Saves grocery list of meal plan to text file
+     * @param mealPlan Meal plan from which the list is generated
+     * @param groceryList List of ingredients to write
+     */
     private void writeGroceryListToFile(MealPlan mealPlan, List<Ingredient> groceryList) {
         String fileName = "grocerylist_" + mealPlan.getID() + ".txt";
         try (FileWriter writer = new FileWriter(fileName)) {
@@ -108,6 +161,9 @@ public class RecipeSystemFACADE {
         }
     }
 
+    /**
+     * Logs out the current user
+     */
     public void logout() {
         UserList.getInstance().save();
     }
