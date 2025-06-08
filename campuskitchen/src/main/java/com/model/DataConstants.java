0357cbafce1db030a43992db707a2289845983de
AshwinSakthivel <ashwin08807@gmail.com>
2025-06-08 13:51:34 -0400

    update

--- campuskitchen/src/main/java/com/model/DataConstants.java
+++ campuskitchen/src/main/java/com/model/DataConstants.java
@@ -1,9 +1,8 @@
 package com.model;
 
 public abstract class DataConstants {
-    
-public class DataConstants {
     public static final String USERS_FILE = "src/main/java/json/Users.json";
     public static final String RECIPES_FILE = "src/main/java/json/Recipes.json";
     public static final String MEALPLANS_FILE = "src/main/java/json/MealPlans.json";
+    
 }
