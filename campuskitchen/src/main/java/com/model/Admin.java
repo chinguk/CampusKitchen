35e7a32abb530be7d6ed449a5a64562bdd79e3d1
Chi Nguyen <chikn@email.sc.edu>
2025-06-08 15:49:10 -0400

    added documentation for Rating, Ingredient, Recipe, Admin classes #60 #59 #61 #57

--- campuskitchen/src/main/java/com/model/Admin.java
+++ campuskitchen/src/main/java/com/model/Admin.java
@@ -1,14 +1,28 @@
 package com.model;
 
+/*
+ * Represents admin user who has privilege to approve submitted recipes
+ * Inherits fields and behaviors of User and adds ability to approve Recipes
+ */
 public class Admin extends User {
     private String adminID;
 
-    // matched parameters to parent
+    /**
+     * Constructs new Admin
+     * @param firstName Admin's first name
+     * @param lastName Admin's last name
+     * @param email Admin's eemail
+     * @param universityID Admin's ID
+     * @param username Admin's username
+     * @param password Admin's passowrd
+     */
     Admin(String firstName, String lastName, String email, String universityID, String username, String password){
         super(firstName, lastName, email, universityID, username, password);
-    
     }
 
+    /*
+     * Approves pending recipe and changing it's status
+     */
     public void approveRecipe(Recipe recipe) {
 
     }
--- campuskitchen/src/main/java/com/model/Ingredient.java
+++ campuskitchen/src/main/java/com/model/Ingredient.java
@@ -1,49 +1,93 @@
 package com.model;
 
+/**
+ * Represents ingredient used in a recipe
+ */
 public class Ingredient {
     
     private String name;
     private double amount;
     private Unit unit;
     
+    /**
+     * Constructs an Ingredient
+     * @param name Ingredient's name
+     * @param amount Quantity of ingredient
+     * @param unit Unit of measurement for amount
+     */
     public Ingredient (String name, double amount, Unit unit) {
         this.name = name;
         this.amount = amount;
         this.unit = unit;
     }
 
+    /**
+     * Returns name of ingredient
+     * @return ingredient name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns quantity of ingredient
+     * @return ingredient amount
+     */
     public double getAmount() {
         return amount;
     }
 
+    /**
+     * Returns Unit of measurement for ingredient amount
+     * @return Unit of measurement
+     */
     public Unit getUnit() {
         return unit;
     }
 
+    /**
+     * Sets new name for ingredient
+     * @param name new ingredient name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Sets new quantity for ingredient
+     * @param amount new ingredient amount
+     */
     public void setAmount(double amount) {
         this.amount = amount;
     }
 
+    /**
+     * Sets new unit of measurement for ingredient
+     * @param unit new unit
+     */
     public void setUnit(Unit unit) {
         this.unit = unit;
     }
 
+    /**
+     * Updates current amount
+     * @param amount new amount to set
+     */
     public void updateAmount(double amount) {
         this.amount = amount;
     }
 
+    /**
+     * Updates current unit of measurement
+     * @param unit New unit to set
+     */
     public void updateUnit(Unit unit) {
         this.unit = unit;
     }
 
+    /**
+     * Returns String representation of ingredient
+     */
     public String toString() {
         return "Ingredient{" + "name='" + name + '\'' + ", amount=" + amount + ", unit=" + unit + '}';
     }
