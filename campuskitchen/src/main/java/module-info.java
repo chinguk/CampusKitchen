6b66d93268a6cab4e041088057ca0c3a18924ad5
KimberlyB6 <brownks6@g.cofc.edu>
2025-06-09 13:17:35 -0400

    tesst cases

--- campuskitchen/src/main/java/module-info.java
+++ campuskitchen/src/main/java/module-info.java
@@ -2,9 +2,12 @@ module com.campus {
     requires javafx.controls;
     requires javafx.fxml;
     requires json.simple;
+    requires junit;
 
     opens com.campus to javafx.fxml;
     exports com.campus;
 
     opens com.controllers to javafx.fxml;
+    
+    opens com.model to javafx.fxml;
 }
