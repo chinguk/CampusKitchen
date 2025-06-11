c871abdd44de0826d228feeee668e2e7a56750b7
AshwinSakthivel <ashwin08807@gmail.com>
2025-06-11 12:46:58 -0400

    fixed DataLoader

--- campuskitchen/src/main/java/com/model/DataLoader.java
+++ campuskitchen/src/main/java/com/model/DataLoader.java
@@ -235,8 +235,10 @@ public class DataLoader {
         }
         return categories;
     }
-
-    public static void main(String[] args) {
+    /*
+     * Test for getUsers and getRecikpes
+     */
+    /*public static void main(String[] args) {
         ArrayList<User> users = DataLoader.getUsers();
         for(User user : users){
          System.out.println(user);
@@ -244,8 +246,8 @@ public class DataLoader {
         /*ArrayList<Recipe> recipes = DataLoader.getRecipes();
         for(Recipe recipe : recipes){
          System.out.println(recipe);
-        }*/
-     }
+        }
+     }*/
 }
     
 
