f1112b522e653c024bf687c72a3817b3bb69e1fb
KimberlyB6 <brownks6@g.cofc.edu>
2025-06-17 13:32:06 -0400

    added recipe page

---
+++ campuskitchen/src/main/java/com/controllers/RecipeController.java
@@ -0,0 +1,40 @@
+package com.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class RecipeController {
+    @FXML
+    private HBox boxMenu;
+
+    @FXML
+    private VBox boxRecipeContainer;
+
+    @FXML
+    private VBox boxRecipePage;
+
+    @FXML
+    private Button btnBrowse;
+
+    @FXML
+    private Button btnCreate;
+
+    @FXML
+    private Button btnHome;
+
+    @FXML
+    private Button btnMealplan;
+
+    @FXML
+    private Button btnRecipe;
+
+    @FXML
+    private Label lblTitle;
+
+    @FXML
+    private ScrollPane paneScroll;
+    
+}
