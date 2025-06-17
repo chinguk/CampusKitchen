92e833742fdde078f5251e479e0b114d00495b2e
KimberlyB6 <brownks6@g.cofc.edu>
2025-06-17 17:26:46 -0400

    recipe

--- campuskitchen/src/main/java/com/controllers/RecipeController.java
+++ campuskitchen/src/main/java/com/controllers/RecipeController.java
@@ -1,21 +1,15 @@
 package com.controllers;
 
 import java.io.IOException;
-<<<<<<< HEAD
 import java.lang.classfile.Label;
+import java.util.ArrayList;
+import java.util.UUID;
+
 import javafx.fxml.FXML;
 import com.campus.App;
 import com.model.Recipe;
 import com.model.RecipeList;
-=======
-import java.util.ArrayList;
-import java.util.UUID;
-
-import com.campus.App;
-import com.model.Recipe;
-import com.model.RecipeList;
 import com.model.User;
->>>>>>> ad6c8e91c3cf0499b6fd5fc58c41ab0d899e9ff0
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -31,7 +25,7 @@ import javafx.scene.control.Label;
 
 public class RecipeController {
 
-    @FXML 
+    @FXML
     private GridPane grid_recipes;
 
     @FXML
@@ -65,9 +59,8 @@ public class RecipeController {
     private ScrollPane paneScroll;
 
     private User user;
+
     private RecipeList recipeList;
-
-
 
     @FXML
     void handleHomeClick(ActionEvent event) {
@@ -88,34 +81,39 @@ public class RecipeController {
         displayUserRecipes();
     }
 
-    private void displayUserRecipes(){
+    private void displayUserRecipes() {
 
         ArrayList<UUID> recipeIds = user.getRecipesIds();
         ArrayList<Recipe> recipes = recipeList.getByIDs(recipeIds);
-        for(int i = 0; i<recipeIds.size(); i++){
+        for (int i = 0; i < recipeIds.size(); i++) {
             Recipe recipe = recipes.get(i);
             VBox vbox = new VBox();
             Label recipeName = new Label(recipe.getName());
             recipeName.setFont(new Font(14));
             vbox.getChildren().add(recipeName);
-            Image image  = new Image(getClass().getResourceAsStream("/images/"));
+            Image image = new Image(getClass().getResourceAsStream("/images/"));
             ImageView image_recipe = new ImageView(image);
             image_recipe.setFitWidth(100);
             image_recipe.setPreserveRatio(true);
             vbox.getChildren().add(image_recipe);
             vbox.getStyleClass().add("recipe-grid-item");
-            grid_recipes.add(vbox, i,0);
+            grid_recipes.add(vbox, i, 0);
         }
 
     }
 
-    private RecipeList recipeList;
+    private HBox createRecipeCard(Recipe recipe) {
+        HBox card = new HBox(10);
+        Label nameLabel = new Label(recipe.getName());
+        card.getChildren().add(nameLabel);
+        return card;
+    }
 
     public void initialize() {
         recipeList = new RecipeList();
 
         for (Recipe recipe : recipeList.getRecipes()) {
-            HBox card = new HBox(recipe);
+            HBox card = createRecipeCard(recipe);
             boxRecipeContainer.getChildren().add(card);
         }
     }
