8f1565308a6f0643c0e942eaf1e54570b6efede0
AshwinSakthivel <ashwin08807@gmail.com>
2025-06-18 10:12:05 -0400

    RecipeController

--- campuskitchen/src/main/java/com/controllers/RecipeController.java
+++ campuskitchen/src/main/java/com/controllers/RecipeController.java
@@ -1,7 +1,6 @@
 package com.controllers;
 
 import java.io.IOException;
-import java.lang.classfile.Label;
 import java.util.ArrayList;
 import java.util.UUID;
 
@@ -12,24 +11,16 @@ import com.model.RecipeList;
 import com.model.User;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.control.Label;
 
 public class RecipeController {
-
-    @FXML
-    private GridPane grid_recipes;
-
-    @FXML
-    private HBox boxMenu;
 
     @FXML
     private VBox boxRecipeContainer;
@@ -78,43 +69,75 @@ public class RecipeController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        
         displayUserRecipes();
     }
 
     private void displayUserRecipes() {
+        if (user == null) {
+            System.out.println("User is null, cannot display recipes");
+            return;
+        }
+        boxRecipeContainer.getChildren().clear();
 
         ArrayList<UUID> recipeIds = user.getRecipesIds();
         ArrayList<Recipe> recipes = recipeList.getByIDs(recipeIds);
-        for (int i = 0; i < recipeIds.size(); i++) {
-            Recipe recipe = recipes.get(i);
-            VBox vbox = new VBox();
-            Label recipeName = new Label(recipe.getName());
-            recipeName.setFont(new Font(14));
-            vbox.getChildren().add(recipeName);
-            Image image = new Image(getClass().getResourceAsStream("/images/"));
-            ImageView image_recipe = new ImageView(image);
-            image_recipe.setFitWidth(100);
-            image_recipe.setPreserveRatio(true);
-            vbox.getChildren().add(image_recipe);
-            vbox.getStyleClass().add("recipe-grid-item");
-            grid_recipes.add(vbox, i, 0);
-        }
 
-    }
-
-    private HBox createRecipeCard(Recipe recipe) {
-        HBox card = new HBox(10);
-        Label name = new Label(recipe.getName());
-        card.getChildren().add(name);
-        boxRecipeContainer.getChildren().add(card);
-    }
-
-    public void initialize() {
-        recipeList = RecipeList().getInstance();
-
-        for (Recipe recipe : recipeList.getRecipes()) {
-            HBox card = createRecipeCard(recipe);
+        for (Recipe recipe : recipes) {
+            VBox card = createRecipeCard(recipe);
             boxRecipeContainer.getChildren().add(card);
         }
+    }
+
+    private VBox createRecipeCard(Recipe recipe) {
+        VBox card = new VBox(5); 
+    
+        
+        ImageView imageView;
+        try {
+            String imagePath = recipe.getImagePath() != null ? recipe.getImagePath() : "placeholder.png";
+            String path = "/images/" + imagePath;
+            Image image = new Image(getClass().getResourceAsStream(path));
+            imageView = new ImageView(image);
+            imageView.setFitWidth(150);
+            imageView.setPreserveRatio(true);
+        } catch (Exception e) {
+            
+            imageView = new ImageView();
+            imageView.setFitWidth(150);
+            imageView.setFitHeight(100);
+        }
+    
+        Label nameLabel = new Label(recipe.getName());
+        nameLabel.getStyleClass().add("recipe-name");
+        Label descLabel = new Label(recipe.getDescription());
+        descLabel.getStyleClass().add("recipe-description");
+        Label durationLabel = new Label("Cook time: " + recipe.getDuration() + " mins");
+        durationLabel.getStyleClass().add("recipe-duration");
+    
+        card.getChildren().addAll(imageView, nameLabel, descLabel, durationLabel);
+        card.getStyleClass().add("recipe-card");
+    
+        return card;
+    }
+    
+    
+
+    public void initialize() {
+        recipeList = RecipeList.getInstance();
+
+        if (user != null) {
+            displayUserRecipes();
+        } else {
+            boxRecipeContainer.getChildren().clear();
+            for (Recipe recipe : recipeList.getRecipes()) {
+                VBox card = createRecipeCard(recipe);
+                boxRecipeContainer.getChildren().add(card);
+            }
+        }
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
