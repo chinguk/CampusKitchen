100a57d44c96b2fd0b87eaf0b81347f3927e948c
AshwinSakthivel <ashwin08807@gmail.com>
2025-06-19 14:00:06 -0400

    a

--- campuskitchen/src/main/java/com/controllers/GroceryListController.java
+++ campuskitchen/src/main/java/com/controllers/GroceryListController.java
@@ -1,11 +1,15 @@
 package com.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
 import com.campus.App;
 
 
@@ -34,6 +38,42 @@ public class GroceryListController {
 
     @FXML
     private Button btnHome;
+
+    @FXML
+    void handleHomeClick(ActionEvent event) {
+        try {
+            App.setRoot("home");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void handleRecipeClick(ActionEvent event) {
+        try {
+            App.setRoot("recipe");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void handleMealPlanClick(ActionEvent event) {
+        try {
+            App.setRoot("mealplan");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void handleBrowseClick(ActionEvent event) {
+        try {
+            App.setRoot("browse");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void initialize() {
 
